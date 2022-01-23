5869da0012cb86a83b2f0ccbbbc8a4b71ab5793e
Caio Monteiro <cainhomo@hotmail.com>
2022-01-23 19:59:16 -0300

    desafio incompleto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Pagination from './Pagination';
+import List from './List';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let pageNum = 1;
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      people: [],
+    }
+    this.getPeople = this.getPeople.bind(this);
+  }
+  getPeople(){
+      return axios.get("https://swapi.dev/api/people/?page=")
+    .then ((response) => {
+      console.log(response.data.results);
+      this.setState( { people: response.data.results } )
+    })
+  }
+
+  componentDidMount(){
+    this.getPeople()
+  }
+  render() {
+    const {people} = this.state;
+    return (
+      <div className="App">
+        <Pagination
+         peoplePerPage={10}
+         totalPeople={82}
+         paginate={pageNum}/>
+        <List people={people} />
+      </div>
+    );
+  }
 }
 
 export default App;
